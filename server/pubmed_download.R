31008f558c60f265ae42a78be5de8bad3ef2fd52
kishoreanekalla <kishorereddyanekalla@gmail.com>
2017-01-10 15:22:27 -0500

    Data storage dir
    Dumps all xml files into meddling directory

--- server/pubmed_download.R
+++ server/pubmed_download.R
@@ -5,20 +5,17 @@ for(i in 1:length(pkgs)){
   if(require(pkgs[i], character.only = TRUE)==FALSE){ install.packages(pkgs[i]);library(pkgs[i], character.only = TRUE)}
   else { library(pkgs[i],character.only = TRUE)}
 }
-
-# updates data 
-
-#baselinedata<-"/home/ubuntu/PubRunner/Data/ftp_download/pubmed/baseline" # keep all files in a dir ?
-#updatesdata<-"/home/ubuntu/PubRunner/Data/ftp_download/pubmed/updates" # else separately stored 
-# baseline xml files 
-url = "ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline" # update files
+setwd("/home/ubuntu/PubRunner/server/medline") # all xml files would be stored in medline dir
+#baselinedata<-"/home/ubuntu/PubRunner/Data/ftp_download/pubmed/baseline" # 
+#updatesdata<-"/home/ubuntu/PubRunner/Data/ftp_download/pubmed/updates" # all files kept in medline dir
+url = "ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline/" # update files
 base_filenames = getURL(url, ftp.use.epsv = FALSE, dirlistonly = TRUE)
 base_filenames<-strsplit(base_filenames, "\n")
 base_filenames = unlist(base_filenames)
 base_filenames
 for (filename1 in base_filenames){
     if (!file.exists(filename1))
-    	download.file(paste(url, filename1, sep = ""), paste( filename1,sep = "")) 
+        download.file(paste(url, filename1, sep = ""), paste( filename1,sep = ""))
 }
 
 url = "ftp://ftp.ncbi.nlm.nih.gov/pubmed/updatefiles/" # update files
@@ -28,5 +25,5 @@ filenames = unlist(filenames)
 filenames
 for (filename in filenames){
     if (!file.exists(filename))
-    	download.file(paste(url, filename, sep = ""), paste( filename,sep = "")) 
+        download.file(paste(url, filename, sep = ""), paste( filename,sep = ""))
 }
