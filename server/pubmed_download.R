b3adee9d28155b9d7c4b7b4dc4a661955c04f0b5
kishoreanekalla <kishorereddyanekalla@gmail.com>
2017-01-10 13:24:24 -0500

    Auto update datasets
    Download newly populated files from ncbi ftp server

--- server/pubmed_download.R
+++ server/pubmed_download.R
@@ -1,12 +1,28 @@
 #!/usr/bin/env RScript
 
-library(Rcurl)
-url = "ftp://ftp.ncbi.nlm.nih.gov/pubmed/updatefiles/"
+library(RCurl)
+# updates data 
+
+baselinedata<-"/home/ubuntu/PubRunner/Data/ftp_download/pubmed/baseline" # keep all files in a dir ?
+updatesdata<-"/home/ubuntu/PubRunner/Data/ftp_download/pubmed/updates" # else separately stored 
+
+# baseline xml files 
+url = "ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline" # update files
+base_filenames = getURL(url, ftp.use.epsv = FALSE, dirlistonly = TRUE)
+base_filenames<-strsplit(base_filenames, "\n")
+base_filenames = unlist(base_filenames)
+base_filenames
+for (filename1 in base_filenames){
+    if (!file.exists(filename1))
+    	download.file(paste(url, filename1, sep = ""), paste( filename1,sep = "")) 
+}
+
+url = "ftp://ftp.ncbi.nlm.nih.gov/pubmed/updatefiles/" # update files
 filenames = getURL(url, ftp.use.epsv = FALSE, dirlistonly = TRUE)
 filenames<-strsplit(filenames, "\n")
 filenames = unlist(filenames)
 filenames
-for (filename in filenames) {
-download.file(paste(url, filename, sep = ""), paste(getwd(), "/", filename,
-sep = ""))
+for (filename in filenames){
+    if (!file.exists(filename))
+    	download.file(paste(url, filename, sep = ""), paste( filename,sep = "")) 
 }
