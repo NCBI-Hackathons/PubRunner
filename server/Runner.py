38f50d511136f8f80658d1fb0bc2b1bb298d1553
Jake Lever <jlever@bcgsc.ca>
2017-08-25 06:56:57 -0700

    Add new Zenodo upload API

--- server/Runner.py
+++ server/Runner.py
@@ -120,16 +120,17 @@ class Runner:
 
         print(r.status_code)
         print(json.dumps(r.json(),indent=2,sort_keys=True))
+        bucket_url = r.json()['links']['bucket']
 
         for f in os.listdir(output):
             src = os.path.join(output, f)
             if os.path.isfile(src):
-                deposition_id = r.json()['id']
-                data = {'filename': src}
-                files = {'file': open(src, 'rb')}
-                r = requests.post('https://sandbox.zenodo.org/api/deposit/depositions/%s/files' % deposition_id,
-                                params={'access_token': ACCESS_TOKEN}, data=data,
-                                files=files)
+                r = requests.put('%s/%s' % (bucket_url,f),
+                                data=open(src, 'rb'),
+                                headers={"Accept":"application/json",
+                                "Authorization":"Bearer %s" % ACCESS_TOKEN,
+                                "Content-Type":"application/octet-stream"})
+
 
         print(r.status_code)
         print(json.dumps(r.json(),indent=2,sort_keys=True))
